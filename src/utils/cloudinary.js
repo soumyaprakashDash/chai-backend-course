b483b6b6e8d308c1e6f05a5d646e238c064882d2
Soumyaprakash Dash <soumyaprakashdash23@gmail.com>
2025-08-29 10:53:05 +0530

    add multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME ,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:'process.env.CLOUDINARY_API_SECRET',   
+    })
+
+
+    const uploadOnCloudniary = async (localFilePath) => {
+        try {
+            if (!localFilePath) return null
+            //upload file on cloudinary
+            const response  = await cloudinary.uploader.upload(localFilePath,{
+                resource_type:"auto",
+        })
+        // file has been uplaod succesfuly 
+        console.log ('file is uploaded on cloudinary', response_url);
+        return response;
+
+    }catch (error){
+
+        fs.unlinkSync(localFilePath);
+        // remove the locally save tempored file as the upload opration got failed 
+
+    }
+}
+export {uploadOnCloudniary};
